e7eb865d5243a1405758e44cd7282fea509c5fbf
Vitor Hugo <boaventurads.vitorhugo@gmail.com>
2024-05-27 17:30:29 -0300

    acrescentado tratamento de exceção geral para entradas inválidas

--- conta-bancaria/src/ContaTerminal.java
+++ conta-bancaria/src/ContaTerminal.java
@@ -7,19 +7,24 @@ public class ContaTerminal {
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
         System.out.println(" <<< SEJA BEM VINDO AO BANCO TERMINAL >>> \n\n");
 
-        System.out.println("INSIRA O NÚMERO DA CONTA: ");
-        int numeroConta = scanner.nextInt();
+        try {
+            System.out.println("INSIRA O NÚMERO DA CONTA: ");
+            int numeroConta = scanner.nextInt();
 
-        System.out.println("INSIRA A AGÊNCIA: ");
-        String agencia = scanner.next();
+            System.out.println("INSIRA A AGÊNCIA: ");
+            String agencia = scanner.next();
 
-        System.out.println("INSIRA O SEU NOME:");
-        String nome = scanner.next();
+            System.out.println("INSIRA O SEU NOME:");
+            String nome = scanner.next();
 
-        System.out.println("INSIRA O SALDO: ");
-        double saldo = scanner.nextDouble();
+            System.out.println("INSIRA O SALDO: ");
+            double saldo = scanner.nextDouble();
 
-        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque");
+            System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque");
+        }catch (Exception e){
+            System.out.println("Entrada inválida!");
+        }
+
 
     }
 
